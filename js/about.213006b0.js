2ec2bdf6891d15df58d06c4bad7e49e2e23e76e6
drsys2005 <121536262+drsys25@users.noreply.github.com>
2022-12-28 17:36:38 +0900

    Add files via upload

---
+++ js/about.213006b0.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkprj2"]=self["webpackChunkprj2"]||[]).push([[443],{7381:function(n,r,u){u.r(r),u.d(r,{default:function(){return l}});var t=u(3396);const a={class:"about"},e=(0,t._)("h1",null,"This is an about page",-1),s=[e];function c(n,r){return(0,t.wg)(),(0,t.iD)("div",a,s)}var i=u(89);const o={},f=(0,i.Z)(o,[["render",c]]);var l=f}}]);
+//# sourceMappingURL=about.213006b0.js.map
